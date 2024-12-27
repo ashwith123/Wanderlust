1df940873eddc267ef1572c96b97e5492872f3ad
ashwith <ashwith9492@gmail.com>
2024-12-27 14:12:12 +0530

    NEW ROUT
    first we create a new button inside the index rout inside form which has method get-  You’re just requesting a new page or a new form for creating a listing, so no data is being sent. This is why you use the GET method.
    
    create a new rout in app.js which render new.ejs
    
    in new.ejs create a form which send data in action - this send data in req.body which can be used write all text,cntry,...and submit it
    
    inside input we give name for everything as it act as a key value pair
    we can acces the data set by using let {title}=req.body.title if name was title
    
    to simplify the process we give name =title[listing]
    
    CREATE ROUT
    
    this handels the post request sent by new ejs form with help of app.post
    all data of listing sent by form is presnt in req.bodty which is sent as new listing to listing
    
    then we save the listings
    
    then rediredt to the index page ie /listings

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 // checks if connection is successfull main is name of function in which connection is being given
 main()
@@ -25,9 +26,37 @@ app.get("/", (req, res) => {
   res.send("hi this is projects Root");
 });
 
+//index rout
 app.get("/listings/", async (req, res) => {
   const allLisitngs = await Listing.find({});
   res.render("../views/listings/index.ejs", { allLisitngs });
+});
+
+//NEW ROUT
+app.get("/listings/new", (req, res) => {
+  res.render("../views/listings/new.ejs");
+});
+
+//CREATE ROUT
+app.post("/listings", async (req, res) => {
+  let listing = req.body.listing;
+  const newlisting = new Listing(listing);
+  await newlisting.save();
+  res.redirect("/listings");
+});
+
+//UPDATE ROUT
+app.get("/lisitngs/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("../views/listings/show.ejs", { listing });
+});
+
+//show rout
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("../views/listings/show.ejs", { listing });
 });
 
 app.listen(7070, () => {
