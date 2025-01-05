41acbb0dbac95cf05144e890b7188b8ac8bdb5a0
abhinay1801 <abhinayreddy0201@gmail.com>
2025-01-05 21:42:07 +0530

    Fixed duplicate routes and handled errors

--- app.js
+++ app.js
@@ -36,9 +36,8 @@ app.get("/listings/", async (req, res, next) => {
   try {
     const allListings = await Listing.find({});
     res.render("../views/listings/index.ejs", { allListings });
-    //console.log(allListings);
   } catch (error) {
-    next(error); // Pass the error to the next middleware (error handler)
+    next(error); // Pass the error to the next middleware
   }
 });
 
@@ -47,7 +46,6 @@ app.get("/listings/new", (req, res) => {
   res.render("../views/listings/new.ejs");
 });
 
-<<<<<<< HEAD
 // Create route
 app.post("/listings", async (req, res, next) => {
   try {
@@ -57,17 +55,6 @@ app.post("/listings", async (req, res, next) => {
     res.redirect("/listings");
   } catch (error) {
     next(error); // Pass the error to the next middleware
-=======
-//CREATE ROUT
-app.post("/listings", async (req, res, next) => {
-  try {
-    let listing = req.body.listing;
-    const newlisting = new Listing(listing);
-    await newlisting.save();
-    res.redirect("/listings");
-  } catch (err) {
-    next(err);
->>>>>>> main
   }
 });
 
@@ -82,7 +69,6 @@ app.get("/listings/:id/edit", async (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
 // Update route
 app.put("/listings/:id", async (req, res, next) => {
   try {
@@ -103,24 +89,6 @@ app.get("/listings/:id", async (req, res, next) => {
   } catch (error) {
     next(error); // Pass the error to the next middleware
   }
-=======
-//UPDATE ROUT
-app.put("/listings/:id", async (err, next, req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect("/listings/${id}");
-});
-
-app.use((next, err, res, req) => {
-  res.send("there is some mistake");
-});
-
-//show rout
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("../views/listings/show.ejs", { listing });
->>>>>>> main
 });
 
 // Delete route
