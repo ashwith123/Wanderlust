4de209fc191732d6455d2bb3ca3be36acdf24c4d
ashwith <ashwith9492@gmail.com>
2025-01-10 19:05:40 +0530

    changes made to schema.js

--- schema.js
+++ schema.js
@@ -1,14 +1,12 @@
-//used for server side validation
-
 const Joi = require("joi");
 
 const listingSchema = Joi.object({
   listing: Joi.object({
-    title: Joi.string.required(),
-    description: Joi.string.required(),
-    location: Joi.string.required(),
-    country: Joi.string.required().min(0),
-    price: Joi.number.required(),
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().min(0).required(), // Ensure `min(0)` comes before `required()`
+    price: Joi.number().required(),
   }).required(),
 });
 
