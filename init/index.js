22dc051e3da3a9d8e2c2e4a29d785b9e93ea14f9
Ashwith reddy <137775785+ashwith123@users.noreply.github.com>
2025-01-19 15:49:19 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,5 +1,8 @@
 //this initially adds data which exists in data.js 
 //ie it is adding data to Listing which is a model we always add data to model schema is just a bluew print
+// listsing.js inside models is creating a model not a schama
+// model is where the data is store. data is not stored in array its stored in model so we first insert inital data to it using index.js
+
 
 const mongoose = require("mongoose");
 const Listing = require("../models/listing");
